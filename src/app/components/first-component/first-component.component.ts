84656754ce9ad52b341bcce5b4ca2fa307c0e7c1
nairo-mudumane <nairomudumane@outlook.com>
2022-11-02 10:13:07 +0200

    [200~interpolation~

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent implements OnInit {
+  firstname: String = 'Nairo';
+  lastname: String = 'Mudumane';
+  fullname: string = `${this.firstname} ${this.lastname}`;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
